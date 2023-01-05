917a93b34d035299daa1cabb1db59170b9fc05c0
kartikchouhann <112246774+kartikchouhann@users.noreply.github.com>
2023-01-05 21:52:20 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 variable "vm_name" {
-  type = string
+  default = "kartik"
 }
 
 variable "machine_type" {
-  type = string
+  default = "e2-micro"
 }
 
 variable "zone" {
-  type = string
+  default = "us-west4-b"
 }
