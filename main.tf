7c4aca0b2d54f309b4e2f66a5cdce354e6c636f0
kartikchouhann <112246774+kartikchouhann@users.noreply.github.com>
2023-01-05 22:15:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "vm" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "c0-deeplearning-common-cpu-v20221215-debian-10"
     }
   }
 
