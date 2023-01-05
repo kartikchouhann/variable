375f08e36de211f9275ee84972b7a056ea436e1c
kartikchouhann <112246774+kartikchouhann@users.noreply.github.com>
2023-01-05 22:01:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "vm" {
   name         = "my-vm"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+  zone         = "us-west4-b"
 
   boot_disk {
     initialize_params {
